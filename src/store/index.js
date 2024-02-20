7091b50f38df8713da50f6c9935588d40b3113dd
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-20 16:58:47 +0100

    Feat index.js - move arrays here

--- src/store/index.js
+++ src/store/index.js
@@ -3,4 +3,57 @@ import { reactive } from "vue";
 export const store = reactive({
   apiUrl: "...",
   apiKey: "...",
+
+  socialLinks: [
+    {
+      icon: "fa-brands fa-facebook-f",
+      url: "#",
+    },
+    {
+      icon: "fa-brands fa-instagram",
+      url: "#",
+    },
+    {
+      icon: "fa-brands fa-twitter",
+      url: "#",
+    },
+  ],
+
+  quickLinks: [
+    {
+      text: "HOME",
+      url: "#",
+    },
+    {
+      text: "BLOG",
+      url: "#",
+    },
+    {
+      text: "GALLERY",
+      url: "#",
+    },
+    {
+      text: "CONTACT",
+      url: "#",
+    },
+  ],
+
+  educationLinks: [
+    {
+      text: "FIXTURES & RESULTS",
+      url: "#",
+    },
+    {
+      text: "LEAGUE TABLE",
+      url: "#",
+    },
+    {
+      text: "PLAYERS",
+      url: "#",
+    },
+    {
+      text: "GALLERY",
+      url: "#",
+    },
+  ],
 });
