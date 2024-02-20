fdb407fe39e81258f7becae570ab3c4f8209cc2e
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-20 09:31:55 +0100

    Feat main.js - icons FontAwesome

--- src/main.js
+++ src/main.js
@@ -4,4 +4,32 @@ import App from "./App.vue";
 
 import * as bootstrap from "bootstrap";
 
-createApp(App).mount("#app");
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faPhoneVolume } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+
+library.add(
+  faTwitter,
+  faChevronLeft,
+  faChevronRight,
+  faLocationDot,
+  faPhoneVolume,
+  faFacebookF,
+  faEnvelope,
+  faInstagram
+);
+
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
