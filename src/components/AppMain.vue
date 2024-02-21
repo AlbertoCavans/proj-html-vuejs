57e26a275490496760f052c623062f194a96568a
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-21 02:20:46 +0100

    Feat AppMain.vue & AppMainSection1.vue - basic structure section1

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,41 @@
 <script>
+import { store } from "../store";
+import AppMainSection1 from "./AppMainSection1.vue";
+
 export default {
   data() {
-    return {};
+    return {
+      store,
+    };
+  },
+
+  methods: {
+    buildImagePath(imageName) {
+      const imageUrl = new URL("../assets/img/" + imageName, import.meta.url);
+      /* console.log(imageUrl); */
+      return imageUrl.href;
+    },
+  },
+
+  components: {
+    AppMainSection1,
   },
 };
 </script>
 
 <template>
-  <h1>AppMain</h1>
+  <div class="main-container">
+    <app-main-section1
+      :homeTeamImg="buildImagePath(store.nextMatch.homeTeamImg)"
+      :guestTeamImg="buildImagePath(store.nextMatch.guestTeamImg)"
+    ></app-main-section1>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../assets/style/partials/mixins" as *;
+
+.main-container {
+  @include centered();
+}
+</style>
---
+++ src/components/AppMainSection1.vue
@@ -0,0 +1,52 @@
+<script>
+import { store } from "../store";
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
+  props: {
+    homeTeamImg: String,
+    guestTeamImg: String,
+  },
+};
+</script>
+
+<template>
+  <div class="next-match-section">
+    <div class="row-teams">
+      <div class="d-flex">
+        <h2>{{ store.nextMatch.homeTeamName }}</h2>
+        <img :src="homeTeamImg" alt="" />
+      </div>
+      <div><h2 class="h1">VS</h2></div>
+      <div>
+        <img :src="guestTeamImg" alt="" />
+        <h2>{{ store.nextMatch.guestTeamName }}</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/style/partials/mixins" as *;
+.next-match-section {
+  @include center-evenly();
+  height: 400px;
+  width: 1000px;
+}
+
+.row-teams {
+  @include center-evenly();
+  width: 800px;
+  div {
+    display: flex;
+    align-items: center;
+  }
+}
+* {
+  overflow: hidden;
+}
+</style>
