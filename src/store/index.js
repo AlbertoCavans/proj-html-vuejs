1b912db98ad1d09e99300fc4a6e08c53207b9136
AlbertoCavans <alberto96cavani@gmail.com>
2024-02-21 02:19:24 +0100

    Feat index.js - arrays teams

--- src/store/index.js
+++ src/store/index.js
@@ -56,4 +56,56 @@ export const store = reactive({
       url: "#",
     },
   ],
+
+  nextMatch: {
+    homeTeamName: "Instanbul Sports",
+    homeTeamImg: "club-1.png",
+    guestTeamName: "UnitedFs Club",
+    guestTeamImg: "club-2.png",
+    dateMatch: "May 21, 2022 18:05 pm",
+    location: "Expert Stadium",
+  },
+
+  upcomingMatchs: [
+    {
+      homeTeamName: "Schalke Club",
+      homeTeamImg: "club-3.png",
+      guestTeamName: "Inter Club",
+      guestTeamImg: "club-4.png",
+      dateMatch: "May 21, 2022 18:05 pm",
+      location: "Expert Stadium",
+    },
+    {
+      homeTeamName: "Atletico Club",
+      homeTeamImg: "club-9.png",
+      guestTeamName: "Schalke Club",
+      guestTeamImg: "club-3.png",
+      dateMatch: "May 21, 2022 18:05 pm",
+      location: "Expert Stadium",
+    },
+    {
+      homeTeamName: "UnitedFs Club",
+      homeTeamImg: "club-2.png",
+      guestTeamName: "Bayern Club",
+      guestTeamImg: "club-8.png",
+      dateMatch: "May 21, 2022 18:05 pm",
+      location: "Expert Stadium",
+    },
+    {
+      homeTeamName: "Arsenal Club",
+      homeTeamImg: "club-7.png",
+      guestTeamName: "Atletico Club",
+      guestTeamImg: "club-9.png",
+      dateMatch: "May 21, 2022 18:05 pm",
+      location: "Expert Stadium",
+    },
+    {
+      homeTeamName: "Instanbul Sports",
+      homeTeamImg: "club-1.png",
+      guestTeamName: "UnitedFs Club",
+      guestTeamImg: "club-2.png",
+      dateMatch: "May 21, 2022 18:05 pm",
+      location: "Expert Stadium",
+    },
+  ],
 });
